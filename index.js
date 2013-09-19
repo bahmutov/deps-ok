cfdce34ffc45e804727236759631c3621058c2b7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-09-18 23:18:41 -0400

    checking if file exists and version is compatible

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var _ = require('lodash');
+var semver = require('semver');
+var check = require('check-types');
 var path = require('path');
 var fs = require('fs');
 var packageFilename = path.join(process.cwd(), 'package.json');
@@ -21,16 +23,51 @@ if (package.peerDependencies) {
   deps = _.extend(deps, package.peerDependencies);
 }
 
+function cleanVersion(version) {
+  check.verifyString(version, 'expecting version string');
+
+  version = version.trim();
+  version = version.replace('~', '');
+  var twoDigitVersion = /^\d+\.\d+$/;
+  if (twoDigitVersion.test(version)) {
+    version += '.0';
+  }
+  version = semver.clean(version);
+
+  return version;
+}
+
 console.log(package.name + ' declares:\n' +
   JSON.stringify(deps, null, 2));
 
 var missing = Object.keys(deps).some(function (dep) {
   var declaredVersion = deps[dep];
+  check.verifyString(declaredVersion, 'missing declared version for ' + dep);
+  declaredVersion = cleanVersion(declaredVersion);
+  check.verifyString(declaredVersion, 'could not clean up version ' + deps[dep]);
+
   var depPackage = path.join(process.cwd(), 'node_modules', dep, 'package.json');
-  console.log('checking', depPackage);
+  // console.log('checking', depPackage);
 
   if (!fs.existsSync(depPackage)) {
     console.error('cannot find module', dep);
+    return true;
+  }
+  var installedDep = require(depPackage);
+  var installedVersion = installedDep.version;
+  if (!_.isString(installedVersion)) {
+    console.error('cannot version for module', dep);
+    return true;
+  }
+  installedVersion = cleanVersion(installedVersion);
+  if (!semver.valid(installedVersion)) {
+    console.error('invalid version', installedVersion, 'for module', dep);
+    return true;
+  }
+  // console.log('comparing', installedVersion, 'with needed', declaredVersion);
+  if (semver.lt(installedVersion, declaredVersion)) {
+    console.error('module', dep, declaredVersion,
+      'needed, but found', installedVersion);
     return true;
   }
 
