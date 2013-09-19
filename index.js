f75d3c3be081f077b04bc7a4d2f4ad3abc4f7614
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-09-18 22:50:37 -0400

    reading current package

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var package = require('./package.json');
+#!/usr/bin/env node
 
-var deps = [].concat(package.dependencies)
-  .concat(package.devDependencies);
-console.log('deps', deps);
+var _ = require('lodash');
+var path = require('path');
+var packageFilename = path.join(process.cwd(), './package.json');
+var package = require(packageFilename);
+
+if (!_.isString(package.name)) {
+  throw new Error('missing package name inside ' + packageFilename);
+}
+
+var deps = {};
+if (package.dependencies) {
+  deps = _.extend(deps, package.dependencies);
+}
+if (package.devDependencies) {
+  deps = _.extend(deps, package.devDependencies);
+}
+if (package.peerDependencies) {
+  deps = _.extend(deps, package.peerDependencies);
+}
+
+console.log(package.name + ' declares:\n' + JSON.stringify(deps, null, 2));
