875cffe6544cd84ee32c92f847bb6d2e7dd0e194
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-02-20 12:19:10 -0500

    added --version command line option

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var argv = require('optimist').argv;
 var check = require('./src/check-folder');
 
 if (!module.parent) {
+  if (argv.version) {
+    var pkg = require('./package.json');
+    console.log(pkg.name, pkg.version);
+    process.exit(0);
+    return;
+  }
   var verbose = argv.verbose;
   var ok = check(process.cwd(), verbose);
   process.exit(ok ? 0 : 1);
