2c4c46038b1f6993b6e8e878772375bb8f33eccc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-02-20 11:47:43 -0500

    checking if package.json exists

--- src/check-folder.js
+++ src/check-folder.js
@@ -1,10 +1,16 @@
 var verify = require('check-types').verify;
 var checkNpm = require('./check-npm-package');
+var join = require('path').join;
+var exists = require('fs').existsSync;
 
 function checkDependenciesInFolder(folder, verbose) {
   verify.unemptyString(folder, 'missing folder string');
-  if (!checkNpm(folder, verbose)) {
-    return false;
+
+  var packageFilename = join(folder, 'package.json');
+  if (exists(packageFilename)) {
+    if (!checkNpm(folder, verbose)) {
+      return false;
+    }
   }
 
   return true;
