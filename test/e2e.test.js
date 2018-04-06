c6574d3f4da39450893ab3bbc532dfaf5726865b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-04-06 11:35:05 -0400

    move more e2e tests

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -4,7 +4,17 @@ const check = require('..')
 const ERROR_EXIT_CODE = 255
 const execaWrap = require('execa-wrap')
 
-describe.only('e2e NPM tests in root folder', () => {
+const expectSuccess = (output) => {
+  output = output.trim()
+  expect(output).toBe("code: 0")
+}
+
+const expectError = (output) => {
+  output = output.trim()
+  expect(output).toBe(`code: ${ERROR_EXIT_CODE}`)
+}
+
+describe('e2e NPM tests in root folder', () => {
   beforeEach(() => {
     process.chdir(relative('..'))
   })
@@ -18,36 +28,42 @@ describe.only('e2e NPM tests in root folder', () => {
       filter: 'code'
     }
     return execaWrap('node', ['bin/deps-ok.js', '--verbose'], options)
-      .then(output => output.trim())
-      .then(output => {
-        expect(output).toBe("code: 0")
-      })
+      .then(expectSuccess)
   })
 })
 
-// gt.module 'e2e NPM tests in this folder'
+describe('e2e NPM tests in this folder', () => {
+  const args = ['../bin/deps-ok.js', '--verbose', '--filename']
 
-// args = ['../bin/deps-ok.js', '--verbose', '--filename']
+  const options = {
+    filter: 'code'
+  }
 
-// gt.async 'test itself with folder', ->
-//   gt.exec 'node', args.concat(relative('..')), 0,
-//     'this module has all dependencies'
+  it('test itself with folder', () => {
+    return execaWrap('node', args.concat(relative('..')), options)
+    .then(expectSuccess)
+  })
 
-// gt.async 'test itself with package.json path', ->
-//   gt.exec 'node', args.concat(relative('../package.json')), 0,
-//     'this module has all dependencies'
+  it('test itself with package.json path', () => {
+    return execaWrap('node', args.concat(relative('../package.json')), options)
+    .then(expectSuccess)
+  })
 
-// gt.async 'test version with latest keyword', ->
-//   gt.exec 'node', args.concat(relative('./package-with-latest.json')), ERROR_EXIT_CODE,
-//     'this has some missing dependencies'
+  it('test version with latest keyword', () => {
+    return execaWrap('node', args.concat(relative('../package-with-latest.json')), options)
+    .then(expectError)
+  })
 
-// gt.async 'test version with github version', ->
-//   gt.exec 'node', args.concat(relative('./package-with-github.json')), 0,
-//     'this handles github: version string'
+  it('test version with github version', () => {
+    return execaWrap('node', args.concat(relative('../package-with-github.json')), options)
+    .then(expectSuccess)
+  })
 
-// gt.async 'test version with local file path', ->
-//   gt.exec 'node', args.concat(relative('./package-with-file.json')), 0,
-//     'this handles file: version string'
+  it('test version with local file path', () => {
+    return execaWrap('node', args.concat(relative('../package-with-file.json')), options)
+    .then(expectSuccess)
+  })
+})
 
 // gt.async 'test version with url', ->
 //   gt.exec 'node', args.concat(relative('./package-with-url.json')), 0,
